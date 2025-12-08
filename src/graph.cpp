7aa33f71b2310be64c5fc8ee1584c358be78e452
Abdullah <179810492+Abdullah-Jabbar786@users.noreply.github.com>
2025-12-08 09:38:00 +0500

    Refactor graph methods and improve performance
    Kept static for performance.

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,7 +1,6 @@
 #include "graph.h"
 
 Graph::Graph() : nodeCount(0) {}
-
 Graph::Graph(int n) : nodeCount(0) { adj.resize(n); }
 
 void Graph::addNode(const string& name) {
@@ -16,11 +15,12 @@ void Graph::addEdge(const string& u, const string& v, int weight) {
     int a = nameToIndex[u];
     int b = nameToIndex[v];
     adj[a].push_back({b, weight});
-    adj[b].push_back({a, weight}); // undirected
+    adj[b].push_back({a, weight}); 
 }
 
 const unordered_map<string, vector<pair<string,int>>>& Graph::getAdjacencyList() const {
-    static unordered_map<string, vector<pair<string,int>>> list;
+    static unordered_map<string, vector<pair<string,int>>> list; // Kept static for performance.
+
     list.clear();
     for(int i=0; i<nodeCount; i++){
         string name = indexToName[i];
