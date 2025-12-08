2023337c6186fb236f3d5883c8645f416aa80888
Abdullah <179810492+Abdullah-Jabbar786@users.noreply.github.com>
2025-12-08 09:06:46 +0500

    Fix formatting and whitespace in astar.h

--- src/astar.h
+++ src/astar.h
@@ -6,7 +6,6 @@
 #include <raylib.h> // For Vector2
 #include "graph.h"
 #include "dijkstra.h" // Reuse PathResult
-
 using namespace std;
 
 PathResult findShortestPathAStar(const Graph& g, const string& start, const string& end, const unordered_map<string, Vector2>& positions);
