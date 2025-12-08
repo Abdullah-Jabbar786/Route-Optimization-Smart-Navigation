2ccd8ca6d19a276f4c62e57cc8f64710414b9a46
Abdullah <179810492+Abdullah-Jabbar786@users.noreply.github.com>
2025-12-08 09:36:23 +0500

    Refactor FoodCourt class to include MainZone and Dijkstra
    Added Dijkstra's algorithm header and updated FoodCourt methods.

--- src/food_court.h
+++ src/food_court.h
@@ -1,26 +1,35 @@
-#pragma once
 #include <raylib.h>
 #include <unordered_map>
 #include <string>
 #include <vector>
 #include "graph.h"
+#include "dijkstra.h"
 using namespace std;
 
-class FoodCourt {
+class MainZone; 
+class FoodCourt{
 public:
     FoodCourt();
     ~FoodCourt();
-    void update();
+    void update(MainZone& mz);
     void draw();
 
     bool isExitClicked() { return exitClicked; }
     void resetExit() { exitClicked = false; }
+    Vector2 getVehiclePos() const { return vehiclePos; }
 
 private:
     Graph graph;
     unordered_map<string, Vector2> nodePos;
     Texture2D bg;
     Vector2 vehiclePos;
+
+    // Interactive State
+    bool showInfo = false;
+    string targetName = "";
+    int distance = 0;
+    float walkingTime = 0.0f;
+    vector<string> currentPath;
 
     // Exit Button Logic
     Rectangle exitBtnRect = {20, 20, 100, 40};
